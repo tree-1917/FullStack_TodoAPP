6c1cfdfb8e2fa56791b4dfb77a8ba42de2bc6263
tree-1917 <gamalm1917@gmail.com>
2024-07-08 14:43:33 +0300

    finish untill 202-Delete Todo

--- routers/todos.py
+++ routers/todos.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append("..")
 
 from typing import Optional
-from fastapi import Depends, HTTPException, APIRouter, Request
+from fastapi import Depends, Form, HTTPException, APIRouter, Request,status
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -12,7 +12,8 @@ from .auth import get_current_user, get_user_exception
 
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
+from starlette.responses import RedirectResponse 
+from starlette import status
 # TODO: Initialize APIRouter with a prefix and tags for the todos endpoint
 router = APIRouter(
     prefix="/todos",
@@ -36,15 +37,70 @@ def get_db():
 
 # TODO: Define the endpoint to read all todos by the current user, returning an HTML response
 @router.get("/", response_class=HTMLResponse)
-async def read_all_by_user(request: Request): 
-    return templates.TemplateResponse("home.html", {"request": request})
+async def read_all_by_user(request: Request, db: Session = Depends(get_db)): 
+    
+    todos = db.query(models.Todos).filter(models.Todos.owner_id == 1).all() 
+    
+    return templates.TemplateResponse("home.html", {"request": request, "todos": todos})
 
 # TODO: Define the endpoint to add a new todo, returning an HTML response
 @router.get("/add-todo", response_class=HTMLResponse)
 async def add_new_todo(request: Request): 
     return templates.TemplateResponse("add-todo.html", {"request": request})
 
+@router.post("/add-todo", response_class=HTMLResponse)
+async def add_new_todo(request: Request, title: str = Form(...), 
+                       description : str = Form(...) , 
+                       priority: int = Form(...) , 
+                       db: Session = Depends(get_db)) :
+    todo_model = models.Todos() 
+    todo_model.title = title 
+    todo_model.description = description 
+    todo_model.priority = priority 
+    todo_model.complete = False 
+    todo_model.owner_id = 1 
+    
+    
+    db.add(todo_model)
+    db.commit() 
+    
+    return RedirectResponse(url="/todos/", status_code=status.HTTP_302_FOUND)
+
 # TODO: Define the endpoint to edit an existing todo, returning an HTML response
-@router.get("/edit-todo", response_class=HTMLResponse)
-async def edit_todo(request: Request): 
-    return templates.TemplateResponse("edit-todo.html", {"request": request})
+@router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
+async def edit_todo(request: Request, todo_id:int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first() 
+    return templates.TemplateResponse("edit-todo.html", {"request": request, "todo" : todo})
+
+@router.post("/edit-todo/{todo_id}", response_class=HTMLResponse)
+async def edit_todo_commit(request: Request, todo_id :int , title : str = Form(...),
+                           description: str = Form(...),
+                           priority: int = Form(...), 
+                           db : Session = Depends(get_db)) : 
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first() 
+    
+    todo_model.title = title
+    todo_model.description = description 
+    todo_model.priority = priority 
+    
+    db.add(todo_model)
+    
+    db.commit() 
+    
+    return RedirectResponse(url="/todos", status_code = status.HTTP_302_FOUND)
+
+@router.get("/delete/{todo_id}")
+async def delete_todo(request: Request, todo_id : int, db: Session = Depends(get_db)): 
+    
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id)\
+        .filter(models.Todos.owner_id == 1).first()
+    
+    if todo_model is None : 
+        return RedirectResponse(url="/todos", status_code=status.HTTP_404_NOT_FOUND)
+    
+    db.query(models.Todos).filter(models.Todos.id == todo_id).delete()
+    
+    db.commit() 
+    
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+    
